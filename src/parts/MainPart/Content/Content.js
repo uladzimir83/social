6c75d614c6f28e809dd63b9a6a2faa034c99f1d0
Uladzimir Abramkin <vladimir83.a@gmail.com>
2021-10-12 09:05:05 +0300

    Added MainPart and Content parts

---
+++ src/parts/MainPart/Content/Content.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Content(props) {
+    return (
+        <div className="main-content"></div>
+    )
+}
+
+export default Content;
---
+++ src/parts/MainPart/MainPart.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Content from './Content/Content';
+import NavBar from './SideBar/NavBar';
+
+function MainPart(props) {
+    return (
+        <main>
+            <div className="container">
+                <div className="main__wrapper">
+                    <NavBar />
+                    <Content />
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default MainPart;
