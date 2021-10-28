3286c60dc029f4b3269cae19c6c8ded49ed1aab2
Uladzimir Abramkin <vladimir83.a@gmail.com>
2021-10-28 15:06:00 +0300

    App: added from Redux - Provider and createStore functions. Added initialReducer.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-
+import React from 'react';
 import Header from './components/Header/Header';
 import MainPart from './components/MainPart/MainPart';
 import {BrowserRouter} from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'; 
 import './scss/styles.scss';
+import { initialReducer } from './reducers/initialReducer';
+
+const store = createStore(initialReducer);
 
 function App(props) {
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <MainPart store={props.store} />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <MainPart store={props.store} />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
