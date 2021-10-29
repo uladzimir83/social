e5f24f4407be17e4d236c94a41e71793c88dce6b
Uladzimir Abramkin <vladimir83.a@gmail.com>
2021-10-29 15:37:37 +0300

    Added action for adding profile status and changed profileReducer

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,13 @@
-export const ADD_PROFILE_DATA = 'ADD_PROFILE_DATA';
+export const ADD_PROFILE_DATA = 'ADD_PROFILE_DATA',
+             ADD_PROFILE_STATUS = 'ADD_PROFILE_STATUS';
 
 
 export const setProfileData = (data) => ({
     type: ADD_PROFILE_DATA,
     payload: data,
+});
+
+export const setProfileStatus = (status) => ({
+    type: ADD_PROFILE_STATUS,
+    status: status,
 });
--- src/reducers/profileReducer.js
+++ src/reducers/profileReducer.js
@@ -1,4 +1,5 @@
 import { ADD_PROFILE_DATA } from '../actions/actions';
+import { ADD_PROFILE_STATUS } from '../actions/actions';
 import { userData } from '../Store/Store';
 
 export const profileReducer = (state = userData, action) => {
@@ -6,6 +7,10 @@ export const profileReducer = (state = userData, action) => {
         case ADD_PROFILE_DATA:
             return {
                 ...state, userData: action.payload
+            };
+        case ADD_PROFILE_STATUS:
+            return {
+                ...state, status: action.status
             };
         default:
             return state;
