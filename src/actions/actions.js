c6d517242b318c2e98ce3698ab221e95050867fc
Uladzimir Abramkin <vladimir83.a@gmail.com>
2021-10-31 22:58:45 +0300

    Added actions and function for reducer for AddNews component

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,8 @@
+import { news } from "../Store/Store";
+
 export const ADD_PROFILE_DATA = 'ADD_PROFILE_DATA',
-             ADD_PROFILE_STATUS = 'ADD_PROFILE_STATUS';
+             ADD_PROFILE_STATUS = 'ADD_PROFILE_STATUS',
+             ADD_PROFILE_NEWS = 'ADD_PROFILE_NEWS';
 
 
 export const setProfileData = (data) => ({
@@ -10,4 +13,9 @@ export const setProfileData = (data) => ({
 export const setProfileStatus = (status) => ({
     type: ADD_PROFILE_STATUS,
     status: status,
+});
+
+export const addProfileNews = (news) => ({
+    type: ADD_PROFILE_NEWS,
+    news: news,
 });
