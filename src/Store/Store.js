c56d52fd9e65b66c769cd49bfc3d105d9db604dd
Uladzimir Abramkin <vladimir83.a@gmail.com>
2021-11-04 12:42:53 +0300

    Removed from Store function addNews

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -126,26 +126,6 @@ export let news = [
     },
 ]
 
-export let addNews = (newsItem) => {
-    let formatDate = () => {
-        const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        
-        let d = new Date(),
-            month = '' + monthNames[(d.getMonth() + 1)],
-            day = '' + d.getDate(),
-            year = d.getFullYear();
-      
-        if (month.length < 2) month = '0' + month;
-        if (day.length < 2) day = '0' + day;
-      
-        return [day, month, year].join(' ');
-    }
-
-    newsItem = {...newsItem, date: formatDate(), photo: userData.photo, name: userData.userName, location: userData.location}
-    news.push(newsItem);
-    rerenderEntireTree();
-}
-
 export default userData;
 
 export const subscriber = (observer) => {
