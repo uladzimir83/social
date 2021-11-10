9cba64d8c59655b79d5a7ba14db808a5c9084c11
Uladzimir Abramkin <vladimir83.a@gmail.com>
2021-11-10 12:41:43 +0300

    Changed initial state for profileReducer

--- src/reducers/profileReducer.js
+++ src/reducers/profileReducer.js
@@ -1,15 +1,19 @@
 import { ADD_PROFILE_DATA } from '../actions/actions';
 import { ADD_PROFILE_STATUS } from '../actions/actions';
 
-export const profileReducer = (state = {user: []}, action) => {
+const userInfo = {
+    userInfo: {}
+};
+
+export const profileReducer = (state = userInfo, action) => {
     switch (action.type) {
         case ADD_PROFILE_DATA:
             return {
-                ...state, user: [...state.user, ...action.data]
+                ...state, userInfo: {...state.userInfo, ...action.data}
             };
         case ADD_PROFILE_STATUS:
             return {
-                ...state, status: action.status
+                ...state, userInfo: {...state.userInfo, status: action.status}
             };
         default:
             return state;
