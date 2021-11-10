a5a9d7701e61eb84957cbfdbd07e0b7f14616c34
Uladzimir Abramkin <vladimir83.a@gmail.com>
2021-11-10 17:29:13 +0300

    Added config object for React player for fix youTube error

--- src/components/MainPart/Profile/ProfileVideo.js
+++ src/components/MainPart/Profile/ProfileVideo.js
@@ -6,7 +6,7 @@ function ProfileVideo() {
     return (
         <div className="profile__data__videos profile__box">
             <h2 className="profile__title">My Videos</h2>
-            <ReactPlayer url="https://www.youtube.com/watch?v=eeubLHILz6o" />
+            <ReactPlayer url="https://www.youtube.com/watch?v=eeubLHILz6o" config={{ youtube: { playerVars: { origin: 'http://localhost:3000' } } }}/>
             <Link className="profile__data__btn" to="Videos">See more...</Link>
         </div>
     )
