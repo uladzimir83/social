f01ec809a3f3f5993cca2b352732f0324937dab7
Uladzimir Abramkin <vladimir83.a@gmail.com>
2021-12-06 15:55:01 +0300

    Auth: divided login and password object, now they have own useState

--- frontend/src/components/Header/Auth/Auth.js
+++ frontend/src/components/Header/Auth/Auth.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
+import { login, registration } from '../../../asyncActions/asyncActions';
 
 const Login = (props) => {
 
     const location = useLocation();
     const isLogin = location.pathname === '/login';
 
+    const [loginUser, setLoginUser] = useState('');
+    const [passwordUser, setPasswordUser] = useState('');
+
+    const submitData = () => {
+        if (isLogin) {
+            login({username: loginUser, password: passwordUser});
+        } else {
+        registration({username: loginUser, password: passwordUser});
+        }
+    }
+
     return (
         <div className="auth__wrapper">
             <h2 className="auth__title">{isLogin ? 'Log In' : 'Sign Up'}</h2>
-            <form className="auth__form" action="POST">
+            <form className="auth__form" onSubmit={(e) => {e.preventDefault()}}>
                 <div className="auth__form__row">
                     <label className="auth__form__label" htmlFor="userName">Name</label>
-                    <input className="auth__form__input" type="text" placeholder="Your name" />
+                    <input className="auth__form__input" onChange={(e) => {setLoginUser(e.target.value)}} value={loginUser} type="text" placeholder="Your name" />
                 </div>
                 <div className="auth__form__row">
                     <label className="auth__form__label" htmlFor="userPass">Password</label>
-                    <input className="auth__form__input" type="password" placeholder="Your password" />
+                    <input className="auth__form__input" onChange={(e) => {setPasswordUser(e.target.value)}} value={passwordUser} type="password" placeholder="Your password" />
                 </div>
                 <div className="auth__form__row auth__form__footer">{
                     isLogin ? <div className="auth__form__question">Don't have an account? <NavLink to="registration">Register!</NavLink></div> : 
                     <div className="auth__form__question">Have an account? <NavLink to="login">Log In</NavLink></div>
                 }
                     
-                    <button className="auth__form__submit" type="submit">{isLogin ? 'Log In' : 'SignUp'}</button>
+                    <button className="auth__form__submit" onClick={submitData} type="submit">{isLogin ? 'Log In' : 'SignUp'}</button>
                 </div>
             </form>
         </div>
