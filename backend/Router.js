276260a051612720896822b8ec35a0f89aa6a247
Uladzimir Abramkin <vladimir83.a@gmail.com>
2021-12-06 15:58:08 +0300

    Renamed authRouter to Router for using for common app routing

---
+++ backend/Router.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import controller from './authController.js';
+import newsController from './newsController.js';
+import authMiddleware from "./middleware/authMiddleware.js";
+
+const router = new Router();
+
+router.post('/registration', [
+    check('username', "Имя пользователя не может быть пустым").notEmpty(),
+    check('password', "Пароль должен быть больше 4 и меньше 10 символов").isLength({min: 5, max: 10})
+], controller.registration);
+router.post('/login', controller.login);
+router.get('/users', authMiddleware, controller.getUsers);
+
+router.post('/news', newsController.addNews);
+router.get('/news', newsController.getAllNews);
+router.get('/news/:id', newsController.getOneNews);
+router.put('/news', newsController.updateNews);
+router.delete('/news/:id', newsController.deleteNews);
+
+export default router;
