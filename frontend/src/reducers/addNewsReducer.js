ffb795e2e7c1e7528e8c7076f8b3450aa754804d
Uladzimir Abramkin <vladimir83.a@gmail.com>
2022-03-07 00:31:04 +0300

    Changed News reducer

--- frontend/src/reducers/addNewsReducer.js
+++ frontend/src/reducers/addNewsReducer.js
@@ -1,15 +1,14 @@
 import { ADD_FULL_NEWS, ADD_PROFILE_NEWS, CLEAN_NEWS_ARRAY } from '../actions/actions';
 
-let news = [];
 
-export const addNewsReducer = (state = news, action) => {
+export const addNewsReducer = (state = [], action) => {
     switch (action.type) {
         case ADD_PROFILE_NEWS:
             return [...state, action.news];
         case ADD_FULL_NEWS:
             return [...state, ...action.news]
         case CLEAN_NEWS_ARRAY:
-            return news
+            return []
         default:
             return state;
     }
