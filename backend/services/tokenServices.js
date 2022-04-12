a352edc61f7d4e47e808bebe39a61ef2804f97f3
Uladzimir Abramkin <vladimir83.a@gmail.com>
2022-04-13 01:22:06 +0300

    Added validation for refresh and access tokens

--- backend/services/tokenServices.js
+++ backend/services/tokenServices.js
@@ -13,6 +13,24 @@ class TokenServices {
         }
     }
 
+    validateAccessToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+
     async saveToken(userId, refreshToken) {
         const tokenData = await TokenModel.findOne({where: {id: userId}});
         if(tokenData) {
